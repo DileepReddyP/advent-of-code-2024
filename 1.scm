bd4b7efdc7fe68966619bc854d97abfe222f58e5
Dileep Reddy <dileepreddyp7@gmail.com>
2024-12-02 02:19:06 -0500

    Advent of Code 2024
    First 2 solutions

---
+++ 1.scm
@@ -0,0 +1,66 @@
+(use-modules (ice-9 peg)
+             (srfi srfi-1)
+             (srfi srfi-26)
+             (ice-9 receive)
+             (ice-9 textual-ports))
+
+(define example-data "3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+")
+
+(define-peg-pattern dataset body
+  (* dataline))
+(define-peg-pattern dataline body
+  (and l1 (+ ws) l2 (ignore "\n")))
+(define-peg-pattern ws none
+  (or "\t" " "))
+(define-peg-pattern l1 all
+  (+ (range #\0 #\9)))
+(define-peg-pattern l2 all
+  (+ (range #\0 #\9)))
+
+(define (parse-data data)
+  (peg:tree (match-pattern dataset data)))
+
+(define (get-half id)
+  (lambda (pair)
+    (string->number (car (assoc-ref pair id)))))
+
+(define (get-lists data)
+  (let* ([parsed-tree (parse-data data)]
+         [l1 (map (get-half 'l1) parsed-tree)]
+         [l2 (map (get-half 'l2) parsed-tree)])
+    (values l1 l2)))
+
+
+(define (get-diffs l1 l2)
+  (let* ([s-l1 (sort l1 <)]
+         [s-l2 (sort l2 <)])
+    (apply + (map (compose abs -) s-l1 s-l2))))
+
+(receive [l1 l2]
+    (get-lists example-data)
+  (get-diffs l1 l2))
+
+
+(define part-1-data (get-string-all (open-file "./1.txt" "r")))
+
+(receive [l1 l2]
+    (get-lists part-1-data)
+  (get-diffs l1 l2))
+
+(define (get-sim-score l1 l2)
+  (apply + (map * l1 (map (lambda (a)
+                            (count (cut = a <>) l2)) l1))))
+
+(receive [l1 l2]
+    (get-lists example-data)
+  (get-sim-score l1 l2))
+
+(receive [l1 l2]
+    (get-lists part-1-data)
+  (get-sim-score l1 l2))
---
+++ 2.scm
@@ -0,0 +1,74 @@
+(use-modules (ice-9 peg)
+             (srfi srfi-1)
+             (srfi srfi-26)
+             (ice-9 receive)
+             (ice-9 textual-ports))
+
+(define example-data
+  "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+")
+(define part-2-data
+  (get-string-all (open-file "./2.txt" "r")))
+
+(define-peg-pattern dataset body
+  (* dataline))
+(define-peg-pattern dataline body
+  (and (+ (or level ws)) (ignore "\n")))
+(define-peg-pattern ws none
+  (or "\t" " "))
+(define-peg-pattern level all
+  (+ (range #\0 #\9)))
+
+(define (parse-data data)
+  (peg:tree (match-pattern dataset data)))
+
+(define (get-number report)
+  (map (compose string->number cadr) report))
+
+(define (get-lists data)
+  (let* ([parsed-tree (parse-data data)]
+         [levels (map get-number parsed-tree)])
+    levels))
+
+(define (in-descending a b)
+  (let ([diff (- a b)])
+    (and (> diff 0) (< diff 4))))
+
+(define (in-ascending a b)
+  (let ([diff (- b a)])
+    (and (> diff 0) (< diff 4))))
+
+(define (is-safe pred report)
+  (cond
+   [(null? report) #t]
+   [(null? (cdr report)) #t]
+   [(not (pred (car report) (cadr report))) #f]
+   [else (is-safe pred (cdr report))]))
+
+(define (solve-2.1 dataset)
+  (apply + (map (lambda (report)
+                  (if (or (is-safe in-ascending report)
+                          (is-safe in-descending report))
+                      1
+                      0))
+                (get-lists dataset))))
+
+(define (combine report)
+  (map (lambda (i)
+         (append (list-head report i) (list-tail report (1+ i))))
+       (iota (length report))))
+
+(define (is-safe-with-dampener pred report)
+  (let [(combinations (combine report))]
+    (any (cut is-safe pred <>) combinations)))
+
+(define (solve-2.2 dataset)
+  (length (filter (lambda (report)
+                    (or (is-safe-with-dampener in-ascending report)
+                        (is-safe-with-dampener in-descending report)))
+                  (get-lists dataset))))
