a033120c31416cab916289bf011bd4787cd80699
Dileep Reddy <dileepreddyp7@gmail.com>
2024-12-12 06:05:49 -0500

    Day 12 cosmetic changes

--- 12.scm
+++ 12.scm
@@ -42,27 +42,17 @@ MMMISSJEEE
              perimeter)])))
 
 (define (count-sides ct)
-  (let ([sides/corners 0])
-    (hash-for-each
-     (lambda (c _)
-       (match-let ([(s sw w nw n ne e se)
-                    (map (lambda (d)
-                           (hash-ref ct (+ c d)))
-                         grid-directions-with-diagonals)])
-         (when (or (and (not n) (not e))
-                   (and n e (not ne)))
-           (set! sides/corners (1+ sides/corners)))
-         (when (or (and (not n) (not w))
-                   (and n w (not nw)))
-           (set! sides/corners (1+ sides/corners)))
-         (when (or (and (not s) (not e))
-                   (and s e (not se)))
-           (set! sides/corners (1+ sides/corners)))
-         (when (or (and (not s) (not w))
-                   (and s w (not sw)))
-           (set! sides/corners (1+ sides/corners)))))
-     ct)
-    sides/corners))
+  (hash-fold
+   (lambda (c _ sides/corners)
+     (match-let ([(s sw w nw n ne e se)
+                  (map (lambda (d) (hash-ref ct (+ c d)))
+                       grid-directions-with-diagonals)])
+       (+ sides/corners
+          (if (or (and (not n) (not e)) (and n e (not ne))) 1 0)
+          (if (or (and (not n) (not w)) (and n w (not nw))) 1 0)
+          (if (or (and (not s) (not e)) (and s e (not se))) 1 0)
+          (if (or (and (not s) (not w)) (and s w (not sw))) 1 0))))
+   0 ct))
 
 (define (fence-price plot-map part2?)
   (hash-fold (lambda (co-ord plant prev)
@@ -73,7 +63,6 @@ MMMISSJEEE
                           [a (hash-count (const #t) ct)])
                      (+ prev (* a (if part2? (count-sides ct) p))))))
              0 plot-map))
-
 
 (define (solve-12.1 dataset)
   (statprof
